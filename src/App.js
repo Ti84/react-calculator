61100cdc932b328f73de80fb13b9828c6beace9c
Shane4666 <sc897@nau.edu>
2020-07-20 13:02:22 -0700

    static calculator.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="display-area">
+        <span>0</span>
+      </section>
+      <section className="buttons">
+        <input type="button" className="calc-btn" value="AC" onClick={() => window.alert('AC')} />
+        <input type="button" className="calc-btn" value="+/-" onClick={() => window.alert('+/-')} />
+        <input type="button" className="calc-btn" value="%" onClick={() => window.alert('%')} />
+        <input type="button" className="calc-btn calc-btn--operator" value="÷" onClick={() => window.alert('÷')} />
+        <input type="button" className="calc-btn" value="7" onClick={() => window.alert('7')} />
+        <input type="button" className="calc-btn" value="8" onClick={() => window.alert('8')} />
+        <input type="button" className="calc-btn" value="9" onClick={() => window.alert('9')} />
+        <input type="button" className="calc-btn calc-btn--operator" value="X" onClick={() => window.alert('X')} />
+        <input type="button" className="calc-btn" value="4" onClick={() => window.alert('4')} />
+        <input type="button" className="calc-btn" value="5" onClick={() => window.alert('5')} />
+        <input type="button" className="calc-btn" value="6" onClick={() => window.alert('6')} />
+        <input type="button" className="calc-btn calc-btn--operator" value="-" onClick={() => window.alert('-')} />
+        <input type="button" className="calc-btn" value="1" onClick={() => window.alert('1')} />
+        <input type="button" className="calc-btn" value="2" onClick={() => window.alert('2')} />
+        <input type="button" className="calc-btn" value="3" onClick={() => window.alert('3')} />
+        <input type="button" className="calc-btn calc-btn--operator" value="+" onClick={() => window.alert('+')} />
+        <input type="button" className="calc-btn" value="0" onClick={() => window.alert('0')} />
+        <input type="button" className="calc-btn calc-btn--annoying-period" value="." onClick={() => window.alert('.')} />
+        <input type="button" className="calc-btn calc-btn--operator" value="=" onClick={() => window.alert('=')} />
+      </section>
     </div>
   );
 }
